4806a4dc904974fe2230eeceba291de316528908
khyati <khyatith@usc.edu>
2017-10-02 23:06:43 -0700

    playing with animations

--- about-us-app/src/AboutDetail.js
+++ about-us-app/src/AboutDetail.js
@@ -3,37 +3,38 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './animation.css';
 
 class AboutDetail extends Component {
-  render() {
-    const start = this.props.start;
-    const max = 36;
-    const numRows = [];
-    for(let i =start; i <= max; i++) {
-        if (numRows.length < 3) {
-            numRows.push(i);
+
+    render() {
+        const start = this.props.start;
+        const max = 36;
+        const numRows = [];
+        for(let i =start; i <= max; i++) {
+            if (numRows.length < 3) {
+                numRows.push(i);
+            }
         }
-    }
-    return (
-        <div className="details">
-    {
-        numRows.map((i) => {
-            return (
-                    <ReactCSSTransitionGroup key={ i } transitionName="slide" transitionEnterTimeout={ 300 } transitionLeaveTimeout={ 300 }>
-                        <div className="container" key={ i }>
-                            <p> Pane {i} </p>
-                            <div className="thumb" >
-                                <p> Thumb {i} </p>
+        return (
+            <div>
+                <ReactCSSTransitionGroup transitionName="slide" transitionEnterTimeout={ 1500 } transitionLeaveTimeout={ 700 }>
+                {
+                    numRows.map((i) => {
+                        return (
+                            <div className="container" key={ i }>
+                                <p> Pane {i} </p>
+                                <div className="thumb" >
+                                    <p> Thumb {i} </p>
+                                </div>
+                                <div className="description">
+                                    <p> Personal Description </p>
+                                </div>
                             </div>
-                            <div className="description">
-                                <p> Personal Description </p>
-                            </div>
-                        </div>
-                    </ReactCSSTransitionGroup>
-            )
-        })
+                        )
+                    })
+                }
+                </ReactCSSTransitionGroup>
+            </div>
+        );
     }
-    </div>
-    );
-  }
 }
 
 export default AboutDetail;
